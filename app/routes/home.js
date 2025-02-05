0e842765d6e3d2bfc5d62a0049f6fa2cdd8a239f
Chyld Medford <chyld.medford@gmail.com>
2025-02-04 19:51:33 -0800

    prettier

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,17 +1,17 @@
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import pkg from '@prisma/client'
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import pkg from "@prisma/client";
 
-const { PrismaClient } = pkg
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const { PrismaClient } = pkg;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const router = express.Router()
-const prisma = new PrismaClient()
+const router = express.Router();
+const prisma = new PrismaClient();
 
-router.get('/', async (req, res) => {
-  res.render('home')
-})
+router.get("/", async (req, res) => {
+  res.render("home");
+});
 
-export default router
+export default router;
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import homeRouter from './app/routes/home.js'
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import homeRouter from "./app/routes/home.js";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const app = express()
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'app/views'))
-app.set('view engine', 'pug')
-app.use('/', homeRouter)
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "app/views"));
+app.set("view engine", "pug");
+app.use("/", homeRouter);
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-})
+  console.log("Server is running on port 3000");
+});
