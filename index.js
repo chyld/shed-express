f1a625c5408ac52af7ee02756890397e60bea600
Chyld Medford <chyld.medford@gmail.com>
2025-02-05 01:03:04 -0800

    basic auth for admin pages

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import basicAuth from "express-basic-auth";
 import homeRouter from "./app/routes/home.js";
 import adminShedRouter from "./app/routes/admin/shed.js";
 
@@ -9,10 +10,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
+const auth = basicAuth({
+  users: { [process.env.ADMIN_USERNAME]: process.env.ADMIN_PASSWORD },
+  challenge: true,
+  realm: "Shed Admin Application",
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "app/views"));
 app.set("view engine", "pug");
+
 app.use("/", homeRouter);
+
+app.use("/admin", auth);
 app.use("/admin/sheds", adminShedRouter);
 
 app.listen(process.env.PORT || 3000, () => {
