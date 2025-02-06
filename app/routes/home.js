7f2eb020e43c6df46d88f78de256511ead3ace4a
Chyld Medford <chyld.medford@gmail.com>
2025-02-06 03:22:00 -0800

    optimizations

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,16 +1,8 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-import pkg from "@prisma/client";
-
-const { PrismaClient } = pkg;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const router = express.Router();
-const prisma = new PrismaClient();
 
-router.get("/", async (req, res) => {
+router.get("/", (req, res) => {
   res.render("home");
 });
 
