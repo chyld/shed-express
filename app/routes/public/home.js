c9dd6e56cc362418b3dc0c85f60b4ef1b321dfab
Chyld Medford <chyld.medford@gmail.com>
2025-02-06 14:04:00 -0800

    added all public pug files

--- app/routes/public/home.js
+++ app/routes/public/home.js
@@ -6,4 +6,28 @@ router.get("/", (req, res) => {
   res.render("public/home");
 });
 
+router.get("/sheds", (req, res) => {
+  res.render("public/sheds");
+});
+
+router.get("/trailers", (req, res) => {
+  res.render("public/trailers");
+});
+
+router.get("/metal-buildings", (req, res) => {
+  res.render("public/metal-buildings");
+});
+
+router.get("/play-sets", (req, res) => {
+  res.render("public/play-sets");
+});
+
+router.get("/contact", (req, res) => {
+  res.render("public/contact");
+});
+
+router.get("/about", (req, res) => {
+  res.render("public/about");
+});
+
 export default router;
