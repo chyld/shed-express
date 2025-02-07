2742f7fb9c9516c8345d8016ef8c7ef6918699ae
Chyld Medford <chyld.medford@gmail.com>
2025-02-06 16:08:03 -0800

    added multiple css files

--- scripts/tailwind.sh
+++ scripts/tailwind.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm ./public/css/*
-npx tailwindcss -i ./app/tailwind/admin.css  -o ./public/css/admin.css
-npx tailwindcss -i ./app/tailwind/public.css -o ./public/css/public.css
-
+npx tailwindcss -i ./app/tailwind/admin.css        -o ./public/css/admin.css
+npx tailwindcss -i ./app/tailwind/template.css     -o ./public/css/template.css
+npx tailwindcss -i ./app/tailwind/generic.css      -o ./public/css/generic.css
+npx tailwindcss -i ./app/tailwind/shed-trailer.css -o ./public/css/shed-trailer.css
